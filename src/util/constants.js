fb502b8b89575b57602ae553073bd65c939e0fd7
Sami Ibrahim <sami-dev@hotmail.com>
2021-12-13 17:23:39 +0300

    Add breakpoints in constants file

--- src/util/constants.js
+++ src/util/constants.js
@@ -9,3 +9,16 @@ export const Fly = keyframes`
         transform: translateY(-15%);
     }
 `
+
+export const BREAKPOINTS = {
+    phone: 600,
+    tablet: 950,
+    laptop: 1300,
+  };
+  
+  export const QUERIES = {
+    phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
+    tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
+    laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
+  };
+  
