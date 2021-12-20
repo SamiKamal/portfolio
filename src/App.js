e626d0f527840501b3a07dcaadae29ec6c9d9159
Sami Ibrahim <sami-dev@hotmail.com>
2021-12-20 19:14:58 +0300

    Initial page transition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { animated, useTransition } from 'react-spring';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import AboutPage from './pages/AboutPage';
@@ -6,17 +7,27 @@ import HomePage from './pages/HomePage';
 import WorkPage from './pages/WorkPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/about" element={<AboutPage/>}/>
-      <Route path="/work/:name" element={<WorkPage/>}/>
+  const location = useLocation()
+  const transitions = useTransition(location, {
+    from: { opacity: 0},
+    enter: { opacity: 1},
+  });
 
-    </Routes>
+  return (
+    <>
+    <NavBar/>
+    {transitions((props, item) => 
+     (<animated.div style={props}>
+        <Routes location={item}>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/work/:name" element={<WorkPage/>}/>
+
+        </Routes>
+      </animated.div>)
+)}
     <Footer/>
-  </BrowserRouter>
+  </>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import App from './App';
 import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles/>
-    <App/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
