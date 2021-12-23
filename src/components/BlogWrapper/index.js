d50e0f703bffb216c5887bbd8a9ab36c41462502
Sami Ibrahim <sami-dev@hotmail.com>
2021-12-23 17:35:49 +0300

    Add spacing to BlogWrapper

--- src/components/BlogWrapper/index.js
+++ src/components/BlogWrapper/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Parag } from '../Texts/Paragraph'
 
 function BlogWrapper({children}) {
     return (
@@ -10,7 +11,13 @@ function BlogWrapper({children}) {
 }
 
 const Wrapper = styled.article`
+    margin: 0 auto;
     max-width: 763px;
+    padding-bottom: 32px;
+
+    ${Parag}{
+        padding-bottom: 22px;
+    }
 `
 
 export default BlogWrapper
