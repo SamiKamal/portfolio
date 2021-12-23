029831bd6284553baf14fe247eda22466e5bea47
Sami Ibrahim <sami-dev@hotmail.com>
2021-12-23 17:36:41 +0300

    Add placeholder "Coming Soon"

--- src/work/ReactCommerce.js
+++ src/work/ReactCommerce.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import BlogWrapper from '../components/BlogWrapper'
+import ImageBlog from '../components/ImageBlog'
+import Heading from '../components/Texts/Heading'
+import Paragraph from '../components/Texts/Paragraph'
+import Image from '../images/didimeetahacker-cover.png'
 
 function ReactCommerce() {
     return (
-        <div>
-            Comming soon
-        </div>
+        <BlogWrapper>
+            <Heading>Coming Soon</Heading>
+        </BlogWrapper>
     )
 }
 
