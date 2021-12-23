0fb31c956216dca18c5396c0cc9e6fcdc132b737
Sami Ibrahim <sami-dev@hotmail.com>
2021-12-23 17:35:36 +0300

    Style ImageBlog

--- src/components/ImageBlog/index.js
+++ src/components/ImageBlog/index.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import {styled} from 'styled-components'
-function ImageBlog({src, alt}) {
+import styled from 'styled-components'
+
+function ImageBlog({src, alt, children}) {
+    console.log(src, alt)
     return (
         <Figure>
             <Image src={src} alt={alt}/>
             <FigCap>
-
+                {children}
             </FigCap>
         </Figure>
     )
 }
 
 const Figure = styled.figure`
+    padding-bottom: 16px;
 `
 
 const Image = styled.img`
+    width: 100%;
 `
 
 const FigCap = styled.figcaption`
+    text-align: center;
+    color: var(--color-grey-text-hover);
+    font-size: ${14/ 16}rem;
+    padding: 16px;
 `
 
 export default ImageBlog
