0d4bcb10230cb46164cc6ba29d71c47952771277
Sami Ibrahim <sami-dev@hotmail.com>
2022-01-09 19:40:00 +0300

    Linters configurations

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,138 +1,137 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Heading from '../../components/Texts/Heading'
-import Paragraph from '../../components/Texts/Paragraph'
-import SubHeading from '../../components/Texts/Sub-Heading'
-import SkullImage from '../../images/Hero main-image.png'
-import didimeetahackerCover from '../../images/didimeetahacker-cover.png'
-import reactcommerceCover from '../../images/react-commerce-cover.png'
-import todoListCover from '../../images/Todo-list-cover.png'
-import BackgroundImage from '../../images/Hero background-image.png'
-import {Fly, QUERIES} from '../../util/constants'
-import WorkCard from '../../components/WorkCard'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Heading from "../../components/Texts/Heading";
+import Paragraph from "../../components/Texts/Paragraph";
+import SubHeading from "../../components/Texts/Sub-Heading";
+import SkullImage from "../../images/Hero main-image.png";
+import didimeetahackerCover from "../../images/didimeetahacker-cover.png";
+import reactcommerceCover from "../../images/react-commerce-cover.png";
+import todoListCover from "../../images/Todo-list-cover.png";
+import BackgroundImage from "../../images/Hero background-image.png";
+import { Fly, QUERIES } from "../../util/constants";
+import WorkCard from "../../components/WorkCard";
 
 function HomePage() {
-    return (
-        <>
-            <HeroWrapper>
-                <TextWrapper>
-                    <Heading>Sami Ibrahim</Heading>
-                    <InnerTextWrapper>
-                        <SubHeading symbol="//">Front-end web developer</SubHeading>
-                        <Paragraph>
-                        Hi, My Name is Sami Ibrahim And I’m a React Front-End Developer Who is Based in Saudi Arabia. 
-                        I Have 2 Years of Experience in Coding, You Can <a href="#work">CHECK MY PROJECTS DOWN BELOW</a>, or <Link to="/about">READ MORE ABOUT ME</Link>
-                        </Paragraph>
-                    </InnerTextWrapper>
-                </TextWrapper>
-                <ImageWrapper>
-                    <Image src={SkullImage}/>
-                </ImageWrapper>
-            </HeroWrapper>
+  return (
+    <>
+      <HeroWrapper>
+        <TextWrapper>
+          <Heading>Sami Ibrahim</Heading>
+          <InnerTextWrapper>
+            <SubHeading symbol="//">Front-end web developer</SubHeading>
+            <Paragraph>
+              Hi, My Name is Sami Ibrahim And I’m a React Front-End Developer Who is Based in Saudi
+              Arabia. I Have 2 Years of Experience in Coding, You Can{" "}
+              <a href="#work">CHECK MY PROJECTS DOWN BELOW</a>, or{" "}
+              <Link to="/about">READ MORE ABOUT ME</Link>
+            </Paragraph>
+          </InnerTextWrapper>
+        </TextWrapper>
+        <ImageWrapper>
+          <Image src={SkullImage} />
+        </ImageWrapper>
+      </HeroWrapper>
 
-            <WorkWrapper id="projects">
-                <Heading>Projects</Heading>
-                <InnerTextWrapper>
-                    <SubHeading symbol="?">Latest projects</SubHeading>
-                </InnerTextWrapper>
-                <WorkList>
-                    <WorkCard
-                    title="Did I meet a hacker"
-                    summary="It’s a website that allow users to search and report hackers they meet in video games."
-                    readMoreLink="/projects/didimeetahacker"
-                    visitLink="https://didimeetahacker.vercel.app/"
-                    githubLink="/"
-                    src={didimeetahackerCover}
-                    stack={["React", "Node.js", "MongoDB"]}
-                    />
+      <WorkWrapper id="projects">
+        <Heading>Projects</Heading>
+        <InnerTextWrapper>
+          <SubHeading symbol="?">Latest projects</SubHeading>
+        </InnerTextWrapper>
+        <WorkList>
+          <WorkCard
+            title="Did I meet a hacker"
+            summary="It’s a website that allow users to search and report hackers they meet in video games."
+            readMoreLink="/projects/didimeetahacker"
+            visitLink="https://didimeetahacker.vercel.app/"
+            githubLink="/"
+            src={didimeetahackerCover}
+            stack={["React", "Node.js", "MongoDB"]}
+          />
 
-                    <WorkCard
-                    title="React Commerce"
-                    summary="a website where you can buy furniture for your home."
-                    readMoreLink="/projects/react-commerce"
-                    visitLink="https://react-commercee.netlify.app/"
-                    githubLink="https://github.com/SamiKamal/React-commerce"
-                    src={reactcommerceCover}
-                    stack={["React", "Redux"]}
-                    />
+          <WorkCard
+            title="React Commerce"
+            summary="a website where you can buy furniture for your home."
+            readMoreLink="/projects/react-commerce"
+            visitLink="https://react-commercee.netlify.app/"
+            githubLink="https://github.com/SamiKamal/React-commerce"
+            src={reactcommerceCover}
+            stack={["React", "Redux"]}
+          />
 
-                    <WorkCard
-                    title="Todo List"
-                    summary="a Todo list website with the ability to add multiple projects. Built in 2019"
-                    visitLink="https://to-do-app-3967f.web.app/"
-                    githubLink="https://github.com/SamiKamal/To-do-app"
-                    src={todoListCover}
-                    stack={["Javascript" , "Firebase"]}
-                    />
-                </WorkList>
-            </WorkWrapper>
-        </>
-    )
+          <WorkCard
+            title="Todo List"
+            summary="a Todo list website with the ability to add multiple projects. Built in 2019"
+            visitLink="https://to-do-app-3967f.web.app/"
+            githubLink="https://github.com/SamiKamal/To-do-app"
+            src={todoListCover}
+            stack={["Javascript", "Firebase"]}
+          />
+        </WorkList>
+      </WorkWrapper>
+    </>
+  );
 }
 
 const HeroWrapper = styled.section`
-    display: flex;
-    align-items: center;
-    background-image: url("${BackgroundImage}");
-    background-repeat: no-repeat;
-    background-size: contain;
-    background-position: top right;
-    margin-right: calc(var(--website-gap) * -1);
-    padding-bottom: 64px;
+  display: flex;
+  align-items: center;
+  background-image: url("${BackgroundImage}");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: top right;
+  margin-right: calc(var(--website-gap) * -1);
+  padding-bottom: 64px;
 
-    @media ${QUERIES.tabletAndSmaller} {
-        padding-top: 128px;
-    }
-
-`
+  @media ${QUERIES.tabletAndSmaller} {
+    padding-top: 128px;
+  }
+`;
 
 const TextWrapper = styled.div`
-    flex: 4;
-    margin-right: var(--website-gap);
+  flex: 4;
+  margin-right: var(--website-gap);
 
-    @media ${QUERIES.laptopAndSmaller} {
-        flex: 5;
-    }
-`
+  @media ${QUERIES.laptopAndSmaller} {
+    flex: 5;
+  }
+`;
 
 const InnerTextWrapper = styled.div`
-    --gap: 80px;
-    padding-left: var(--gap);
-    max-width: calc(660px + var(--gap));
+  --gap: 80px;
+  padding-left: var(--gap);
+  max-width: calc(660px + var(--gap));
 
-    @media ${QUERIES.tabletAndSmaller} {
-        --gap: 32px;
-    }
-
-`
+  @media ${QUERIES.tabletAndSmaller} {
+    --gap: 32px;
+  }
+`;
 
 const ImageWrapper = styled.div`
-    flex: 3;
-    flex-shrink: 3;
+  flex: 3;
+  flex-shrink: 3;
 
-    @media ${QUERIES.tabletAndSmaller} {
-        display: none;
-    }
-
-`
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
 
 const Image = styled.img`
-    transform: translateY(-10%);
-    animation: ${Fly} 5s infinite alternate ease-in-out;
-    pointer-events: none;
-`
+  transform: translateY(-10%);
+  animation: ${Fly} 5s infinite alternate ease-in-out;
+  pointer-events: none;
+`;
 
 const WorkWrapper = styled.section`
-    padding: 64px 0;
-`
+  padding: 64px 0;
+`;
 
 const WorkList = styled.div`
-    display: grid;
-    padding-top: 128px;
-    justify-items: center;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-gap: 64px 256px;
-`
+  display: grid;
+  padding-top: 128px;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 64px 256px;
+`;
 
-export default HomePage
+export default HomePage;
--- src/pages/WorkPage/index.js
+++ src/pages/WorkPage/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import DidImeetahacker from '../../work/DidImeetahacker'
-import ReactCommerce from '../../work/ReactCommerce'
+import React from "react";
+import { useParams } from "react-router-dom";
+import DidImeetahacker from "../../work/DidImeetahacker";
+import ReactCommerce from "../../work/ReactCommerce";
 
 function WorkPage() {
-    const {name} = useParams()
+  const { name } = useParams();
 
-    if (name === 'react-commerce'){
-        return <ReactCommerce/>
-    } else if (name === 'didimeetahacker'){
-        return <DidImeetahacker/>
-    } else {
-        return <h1>Wrong project, please make sure to select the right one</h1>
-    }
+  if (name === "react-commerce") {
+    return <ReactCommerce />;
+  }
+  if (name === "didimeetahacker") {
+    return <DidImeetahacker />;
+  }
+  return <h1>Wrong project, please make sure to select the right one</h1>;
 }
 
-export default WorkPage
+export default WorkPage;
